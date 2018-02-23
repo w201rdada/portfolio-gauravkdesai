0536ea4c62a83efaa7ebc9ab0bfd668c8cec54fb
Gaurav Desai <gaurav.desai@berkeley.edu>
2018-02-23 23:22:46 +0530

    deploy

--- _build.sh
+++ _build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd . && rm -rf _book/ _main*md \
+cd ~/Documents/GitHub/portfolio-gauravkdesai && rm -rf _book/ _main*md \
 && Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
